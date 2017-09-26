b04aa342fddf0b5c691437deb6bd1a7207972971
Mottelz <shachna@gmail.com>
2017-09-26 14:58:12 -0400

    removed folders

--- main.cpp
+++ main.cpp
@@ -1,21 +1,75 @@
+//map driver
+
+
 #include <iostream>
+#include "Map.h"
+#include "MapLoader.h"
+
 using namespace std;
 
 int main() {
-    int numOfLanguages;
+	//MapLoader worldFile("World.map");
+	//Map worldMap;
+	//worldFile.readMapFile(worldMap);
+	//worldMap.displayConti();
+	//worldMap.displayTerri();
 
-    cout << "Hello reader.\n"
-         << "Welcome to C++.\n";
+	// worldFile.testMap();
+	vector<string> mapList;
+	vector<int> errorIDList;
+	mapList.push_back("World.map");
+	mapList.push_back("Earth.map");
+	//error maps
+	mapList.push_back("Invalid.map");
+	mapList.push_back("Invalid2.map");
+	//doesnt exist
+	mapList.push_back("invalid3.map");
 
-    cout << "How many programming lanuages have you used? ";
-    cin >> numOfLanguages;
+	mapList.push_back("Canada.map");
+	
+	vector<Map> MAPS;
 
-    if (numOfLanguages < 1) {
-        cout << "Read the preface!";
-    }
-    else {
-        cout << "Enjoy the book.\n";
-    }
+	MapLoader maploader;
 
-    return 0;
+	//Map Earth;
+	for(int i=0;i<mapList.size();i++)
+		MAPS.push_back( Map(mapList[i]));
+	//int offset = 0;
+	for (int i = 0; i < mapList.size(); i++)
+	{
+		//Map temp;
+		try {
+			maploader.readMapFile(mapList[i], MAPS[i]);
+			//MAPS[i].toString();
+		}
+		catch ( const char *e) {
+			cout << "*****************************************" << endl;
+			cout << "Failed loading map file: " << MAPS[i].getMapNam() << endl;
+			cout << e << endl;
+			cout << "*****************************************" << endl;
+			cout << endl;
+			errorIDList.push_back(i);
+		}
+	}
+
+	
+
+	//very important
+	//for (int i = errorIDList.size() - 1; i >= 0; i--)
+	//{
+	//	MAPS.erase(MAPS.begin()+errorIDList[i]);
+	//}
+
+	for (int i = 0; i < MAPS.size(); i++)
+	{
+		//if no MAPS.erase() above then you need this if statement
+		if(MAPS[i].isValid())
+		MAPS[i].toString();
+	}
+
+	cout << "Number of the invalid map files " << errorIDList.size() << endl;
+	cout << endl;
+	system("PAUSE");
+
+	return 0;
 }
