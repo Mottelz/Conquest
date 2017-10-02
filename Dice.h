3db203af30d2bd816a0a2f05251de8e0af50804c
VickelLeung <vickel1993@hotmail.com>
2017-10-02 12:47:03 -0400

    updated Dice.h
    Changed format of DiceCup & Die such as private/public instance variables & methods.

--- Dice.h
+++ Dice.h
@@ -11,40 +11,39 @@ public:
 	//function delcaration
 	int getValue();
 	void setValue(int value);
+	int getDiceCounter();
+	void setDiceCounter(int dc);
+	int rollDice();
 
 private:
 	//instance variables
 	int m_Value;
+	int m_DiceCounter;
 };
 
-class DiceCup {
+class DiceCup : public Die {
 
-//declaration of dice
+	//declaration of dice
 public:
 	//ctor declaration
 	DiceCup();
-	DiceCup(int numberOfDice, int diceCounter);
+	DiceCup(int numberOfDice);
 
 	//getters & setters declaration
 	int getNumberOfDice();
 	void setNumberOfDice(int numberOfDice);
 
-	void setDiceCounter(int diceCounter);
-	int  setDiceCounter();
-
 	//function delcaration
-	std::vector<int> rollDice();
+	/*std::vector<int> rollDice();*/
 	void keepTrack(int x);
-	void initializeArray();
 	void showPercentage();
+	void displayCup();
+	void resetContainer();
+	void storeDiceValue(int x);
 
 private:
 	//instances variables
 	int m_NumberOfDice;
-	int m_DiceCounter;
 	int m_TrackPercent[6];
 	std::vector<int> m_Container;
-	
-	
 };
-
